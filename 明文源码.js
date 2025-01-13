c9ef620c9cf4adc1b4a73629fab5bd67197a9b8d
YUYETIME <abc2549107641@163.com>
2025-01-13 21:07:23 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -6085,7 +6085,7 @@ async function handlePanel(request, env) {
 __name(handlePanel, "handlePanel");
 async function fallback(request) {
   const url = new URL(request.url);
-  url.hostname = "yudxz.pages.dev/";
+  url.hostname = "minecraft.wiki";
   url.protocol = "https:";
   request = new Request(url, request);
   return await fetch(request);
